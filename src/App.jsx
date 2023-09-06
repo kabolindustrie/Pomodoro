0a27a5cb1bd504318e3e07d0af7076dd52f58307
Kabol Amine <amine.kabol@gmail.com>
2023-09-06 15:32:17 +0200

    mise en place de laffichage avec des valeurs statics pour le moment et avec tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 function App() {
 
   return (
-    <div>
-
+    <div className="bg-slate-700 text-slate-100 pt-20 min-h-screen">
+      <div className="max-w-xl mx-auto border border-slate-500 rounded p-10">
+        <h1 className="text-center text-3xl mb-8"> Pomodoro App</h1>
+        <div className="flex justify-center mb-8">
+          {/* {Session block} */}
+          <div className="mr-10">
+            <p className="text-center mb-1">Sessions</p>
+            <div className="flex">
+              <p className="mx-4 text-xl">25</p>
+            </div>
+          </div>
+          {/* {Pause block} */}
+          <div>
+            <p className="text-center mb-1">Pause</p>
+            <div className="flex">
+              <p className="mx-4 text-xl">5</p>
+            </div>
+          </div>
+        </div>
+        <p className="text-center mb-2 text-xl font-semibold">Work</p>
+        <p className="text-center flex justify-center mb-1">
+          <span className="text-4xl p-4 rounded bg-slate-300 text-slate-900">25:00</span>
+        </p>
+        <p className="text-center">Passed cycle(s) : 0</p>
+      </div>
     </div>
   )
 }
