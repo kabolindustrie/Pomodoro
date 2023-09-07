93719933db86c7a4589c9d3a95c27815945e030c
Kabol Amine <amine.kabol@gmail.com>
2023-09-07 18:25:52 +0200

    mise en place de redux avec un store une feature (chrono)

---
+++ src/features/chrono.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialize = {}
+
+export const chrono = createSlice({
+  name: "chrono",
+  initialize,
+  reducer: {
+
+  }
+})
+
+export default chrono.reducer
